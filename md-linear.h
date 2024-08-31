226f572c2c5df45240401b8d4248696ce0128057
Doug Johnson <dougvj@gmail.com>
2024-08-31 12:47:55 -0600

    Initial

---
+++ md-linear.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINEAR_H
+#define _LINEAR_H
+
+#define LEVEL_LINEAR    (-1)
+
+struct dev_info {
+	struct md_rdev	*rdev;
+	sector_t	end_sector;
+};
+
+struct linear_conf
+{
+	struct rcu_head		rcu;
+	sector_t		array_sectors;
+	int			raid_disks; /* a copy of mddev->raid_disks */
+	struct dev_info		disks[] __counted_by(raid_disks);
+};
+#endif
